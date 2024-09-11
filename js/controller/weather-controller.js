6c57b8136f09721399d0e2e0b0e8134632531716
Petrakowww <Petrakov.Egor.G@yandex.ru>
2024-09-11 11:36:41 +0300

    MVC arch in pr

---
+++ js/controller/weather-controller.js
@@ -0,0 +1,8 @@
+// принимает запросы от пользователя и обращается к модели
+
+class WeatherController {
+    constructor(model, view) {
+        this.model = model;
+        this.view = view;
+    }
+}

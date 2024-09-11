c84b17903def733736ffa02f2b18904c815a3dac
Petrakowww <Petrakov.Egor.G@yandex.ru>
2024-09-11 12:52:47 +0300

    making view

--- js/model/weather-model.js
+++ js/model/weather-model.js
@@ -1,1 +1,14 @@
 // Бизнес логика
+
+class WeatherModel {
+    constructor(baseUrl, apiKey){
+        this.apiData = {
+            url : baseUrl, 
+            api: apiKey
+        }
+    }
+
+    async fetchWeatherData(queryParams) {
+        appid
+    }
+}
--- js/view/weather-view.js
+++ js/view/weather-view.js
@@ -1,1 +1,21 @@
 // Отображение данных
+
+class WeatherView {
+    constructor(){
+        this.app = this.getElement('#card');
+    }
+
+    getElement(selectors){
+        return document.querySelector(selectors);
+    }
+
+    deleteClass(selectorClass){
+        const element = this.getElement('#select-setting');
+        element.classList.remove(selectorClass);
+    }
+
+    addClass(selectorClass){
+        const element = this.getElement('#select-setting');
+        element.classList.add(selectorClass);
+    }
+}
